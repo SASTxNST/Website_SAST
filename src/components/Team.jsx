2793702a10d6bde183d5b6f3cd473377382295ab
Prabhav <jainprabhav1437@gmail.com>
2025-06-21 06:46:01 +0000

    added members in the team page

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -29,18 +29,56 @@ const Team = () => {
         <div className="w-300 h-150 flex justify-evenly items-center flex-wrap gap-15" style={{marginTop:"10%"}}>
             <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
                 <div className="w-80 h-90">
-                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                    <img className="object-fill w-80 h-90 rounded" src={https://nebula.sastclub.tech/_next/image?url=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F67745953%3Fs%3D400%26u%3Dcc8f4cb6d463c2ee4c1cc756522e79929dc65ee2%26v%3D4&w=128&q=75} alt="person1"/>
                 </div>
                 <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
-                    <h2 className="font-bold text-l">Thomas Rösler</h2>
-                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
-                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
-                        and gets to the bottom of any question until he strikes
-                        gold.
+                    <h2 className="font-bold text-l">Neelanshu Karn</h2>
+                    <h2 className="font-bold text-l">President</h2>
+                    <p className="text-xs text-center">Leads the Nebula project with vision and technical insight, 
+                        driving the team toward impactful innovation in space-tech.
                     </p>
                 </div>
             </div>
 
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={https://nebula.sastclub.tech/_next/image?url=https%3A%2F%2Fd3dyfaf3iutrxo.cloudfront.net%2Fthumbnail%2Fuser%2Fb1707d706f294124ab883d476518f6b4.jpeg&w=128&q=75} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Pratiti Paul</h2>
+                    <h2 className="font-bold text-l">Content and Documentation</h2>
+                    <p className="text-xs text-center">Supports project leadership with coordination, planning, 
+                        and creative contributions to team direction.
+                    </p>
+                </div>
+            </div>
+
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={https://nebula.sastclub.tech/_next/image?url=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F178926947%3Fv%3D4&w=128&q=75} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Kavya Katal</h2>
+                    <h2 className="font-bold text-l">Vie-President</h2>
+                    <p className="text-xs text-center">Dedicated to full-stack development with MERN,
+                        solving problems through code and creativity.
+                    </p>
+                </div>
+            </div>
+
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={https://nebula.sastclub.tech/_next/image?url=https%3A%2F%2Fd3dyfaf3iutrxo.cloudfront.net%2Fthumbnail%2Fuser%2F8bd8e4c4b408476abf043bdf7d652527.jpeg&w=128&q=75} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Pratya Silla</h2>
+                    <h2 className="font-bold text-l">UI/UX Lead</h2>
+                    <p className="text-xs text-center">Designs intuitive and engaging user interfaces
+                        that enhance user experience across all touchpoints
+                    </p>
+                </div>
+            </div>
+            
             <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
                 <div className="w-80 h-90">
                     <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
@@ -54,7 +92,6 @@ const Team = () => {
                     </p>
                 </div>
             </div>
-
             <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
                 <div className="w-80 h-90">
                     <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
@@ -68,7 +105,71 @@ const Team = () => {
                     </p>
                 </div>
             </div>
-
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Thomas Rösler</h2>
+                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
+                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
+                        and gets to the bottom of any question until he strikes
+                        gold.
+                    </p>
+                </div>
+            </div>
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Thomas Rösler</h2>
+                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
+                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
+                        and gets to the bottom of any question until he strikes
+                        gold.
+                    </p>
+                </div>
+            </div>
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Thomas Rösler</h2>
+                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
+                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
+                        and gets to the bottom of any question until he strikes
+                        gold.
+                    </p>
+                </div>
+            </div>
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Thomas Rösler</h2>
+                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
+                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
+                        and gets to the bottom of any question until he strikes
+                        gold.
+                    </p>
+                </div>
+            </div>
+            <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
+                <div className="w-80 h-90">
+                    <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
+                </div>
+                <div className="h-35 w-80 flex flex-col justify-center items-center gap-2">
+                    <h2 className="font-bold text-l">Thomas Rösler</h2>
+                    <h2 className="font-bold text-l">Captain & Treasure Hunter</h2>
+                    <p className="text-xs text-center">Creates tile jewels, devises a solution to every problem
+                        and gets to the bottom of any question until he strikes
+                        gold.
+                    </p>
+                </div>
+            </div>
             <div className="h-130 w-85 bg-[rgb(0,0,0,0.3)] flex flex-col justify-evenly items-center hover:scale-101 transition duration-200">
                 <div className="w-80 h-90">
                     <img className="object-fill w-80 h-90 rounded" src={person1_png} alt="person1"/>
